9398d4d463b9e7823ee6457ca4611d791c9535b1
Dawid Damek <dawid.damek98@gmail.com>
2023-01-17 16:21:32 +0100

    wip: i18n

--- app/pods/components/product-add/component.js
+++ app/pods/components/product-add/component.js
@@ -1,12 +1,18 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
 export default class ProductAddComponent extends Component {
-  @tracked productNumber;
+  @tracked productNumber = 0;
+  @service intl;
 
   @action
   setProductNumber({ target: { value } }) {
+    if (value < 0) {
+      return;
+    }
     this.productNumber = value;
+    console.log(this.intl.locale);
   }
 }
