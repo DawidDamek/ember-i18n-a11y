54c376506804e3fdc70cd918f4cefa525d98386c
Dawid Damek <dawid.damek98@gmail.com>
2023-01-19 23:46:36 +0100

    highlight not accessible elements

--- app/pods/home/controller.js
+++ app/pods/home/controller.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { storageFor } from 'ember-local-storage';
 import { inject as service } from '@ember/service';
+import axe from 'axe-core';
+import { scheduleOnce } from '@ember/runloop';
 
 export default class HomeController extends Controller {
   @service intl;
@@ -29,5 +31,27 @@ export default class HomeController extends Controller {
   async setLightMode() {
     await this.settings.set('language', false);
     document.body.classList.remove('darkmode');
+  }
+
+  @action
+  auditA11Y() {
+    // eslint-disable-next-line ember/no-incorrect-calls-with-inline-anonymous-functions
+    scheduleOnce('afterRender', this, () => {
+      console.log('dasad');
+      axe.run().then(({ violations }) => {
+        if (violations.length) {
+          violations.map(
+            ({ description, id, impact, help, helpUrl, nodes = [] }) => {
+              nodes.map(({ target }) => {
+                const element = document.querySelector(target);
+                console.log(id, description, help, helpUrl, impact, element);
+                element.style.background = 'red';
+              });
+            }
+          );
+          throw new Error('Accessibility issues found');
+        }
+      });
+    });
   }
 }
